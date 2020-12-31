e31fbd6cae288110e83d5103aaca8f44cd715fa8
ssrout <smruti.sambit.rout@gmail.com>
2020-12-31 14:20:07 +0530

    snake game food

--- Day20/main.py
+++ Day20/main.py
@@ -1,6 +1,9 @@
 from turtle import Screen
+from food import Food
 from snake import Snake
+from score import Score
 import time
+
 screen=Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
@@ -8,6 +11,8 @@ screen.title("Snake Ninja")
 screen.tracer(0)
 
 snake=Snake()
+food=Food()
+score=Score()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -21,5 +26,14 @@ while game_on:
     screen.update()
     time.sleep(1)
     snake.move()  
+    #detect food collision
+    if snake.head.distance(food)<15:
+        score.increase_score()
+        food.refresh()
+
+    #detect wall collision
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
+        game_on=False
+        score.game_over()
 
 screen.exitonclick()
