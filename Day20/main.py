c2bcdafb2138217d6e17b89b577a54e852fd4e09
ssrout <smruti.sambit.rout@gmail.com>
2021-01-01 12:54:05 +0530

    snake game end condition added

--- Day20/main.py
+++ Day20/main.py
@@ -1,39 +1,52 @@
 from turtle import Screen
-from food import Food
 from snake import Snake
-from score import Score
+from food import Food
+from score import Scoreboard
 import time
 
-screen=Screen()
-screen.setup(width=600,height=600)
+screen = Screen()
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Ninja")
+screen.title("My Snake Game")
 screen.tracer(0)
 
-snake=Snake()
-food=Food()
-score=Score()
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-game_on=True
-
-while game_on:
+game_is_on = True
+while game_is_on:
     screen.update()
-    time.sleep(1)
-    snake.move()  
-    #detect food collision
-    if snake.head.distance(food)<15:
-        score.increase_score()
-        food.refresh()
+    time.sleep(0.1)
+    snake.move()
 
-    #detect wall collision
-    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
-        game_on=False
-        score.game_over()
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+
+
 
 screen.exitonclick()
--- Day20/snake.py
+++ Day20/snake.py
@@ -1,41 +1,52 @@
 from turtle import Turtle
-STARTING_POSITION=[(0,0),(-20,0),(-40,0)]
-MOVE_DIST=20
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
 class Snake:
 
     def __init__(self):
-        self.segments=[]
+        self.segments = []
         self.create_snake()
-        self.head=self.segments[0]
+        self.head = self.segments[0]
 
     def create_snake(self):
-        for pos in STARTING_POSITION:
-            sn=Turtle(shape="square")
-            sn.color("red")
-            sn.penup()
-            sn.goto(pos)
-            self.segments.append(sn)
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("yellow")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
-        for seg in range(len(self.segments)-1,0,-1):
-            x_cor=self.segments[seg-1].xcor()
-            y_cor=self.segments[seg-1].ycor()
-            self.segments[seg].goto(x_cor,y_cor)
-        self.head.forward(MOVE_DIST)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.head.heading()!=270:
-            self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        if self.head.heading()!=90:
-            self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        if self.head.heading()!=0:
-            self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        if self.head.heading()!=180:
-             self.head.setheading(0)    
-
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
