69cc257583667448e8e34b87b66238e06cb14358
ssrout <smruti.sambit.rout@gmail.com>
2021-01-21 12:59:18 +0530

    exception handeled

--- Day26/NATO_alphabet/main.py
+++ Day26/NATO_alphabet/main.py
@@ -2,9 +2,18 @@ import pandas
 data=pandas.read_csv("Day26/NATO_alphabet/nato_phonetic_alphabet.csv")
 #create Dictionary
 phonetic_dict={row.letter:row.code for (index,row) in data.iterrows()}
+
 #print(phonetic_dict)
 
 #create phonetic code
-word=input("enter word\n").upper()
-op=[phonetic_dict[letter] for letter in word]
-print(op)
+def gen():
+    try:
+        word=input("enter word\n").upper()
+        op=[phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("only alphabet please")
+        gen()
+    else:
+        print(op)
+
+gen()
