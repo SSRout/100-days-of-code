f57cb061239e1d935e9b6f4ef8fb969410e697f7
ssrout <smruti.sambit.rout@gmail.com>
2021-01-27 16:30:27 +0530

    d56 update

--- Day56/server.py
+++ Day56/server.py
@@ -1,10 +1,11 @@
 from flask import Flask,render_template
-
+import random
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    random_num=random.randint(1,100)
+    return render_template("index.html",num=random_num)
 
 if __name__=="__main__":
     app.run(debug=True)
